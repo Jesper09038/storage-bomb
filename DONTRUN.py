70f80d88c18ad9b69cd0dc4c68f191285f7dfe79
Jesper09038 <156309610+Jesper09038@users.noreply.github.com>
2024-06-24 19:27:50 +0200

    DONTRUN.py
    greater efficienty

--- DONTRUN.py
+++ DONTRUN.py
@@ -11,12 +11,12 @@ def generate_image(image_index, width, height, base_dir):
     array = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
     img = Image.fromarray(array, 'RGB')
     folder_name = f'folder{image_index}'
-    folder_path = os.path.join(base_dir, folder_name)
+    folder_path = "generated_images"
     create_directory(folder_path)
     filename = f'large_image_{image_index}.bmp'
     file_path = os.path.join(folder_path, filename)
     img.save(file_path)
-    print(f'Generated and saved {file_path} with dimensions {width}x{height}')
+    print(f'Generated and saved with dimensions {width}x{height}')
 def main():
     num_images = 1700
     width, height = 8000, 8000
